8fe4203bf0a0201d93a366098c4983828bb8de24
Benjamin Petersen <bpeters7@utk.edu>
2014-04-16 12:45:38 -0400

    initial commit added bash configuration, bash, git, ssh

---
+++ tools/link_to_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -sf $HOME/dotfiles/bash/bash_profile $HOME/.bash_profile
+ln -sf $HOME/dotfiles/bash/bashrc $HOME/.bashrc
+
+#sh
+ln -sf $HOME/dotfiles/ssh/config $HOME/.ssh/config
+
+#git
+ln -sf $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
