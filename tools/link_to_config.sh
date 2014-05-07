b31a432a189137473db0843ae4dd6fad579c6b60
Benjamin Petersen <bpeters7@utk.edu>
2014-05-07 09:18:11 -0400

    Updating and adding files

--- tools/link_to_config.sh
+++ tools/link_to_config.sh
@@ -8,3 +8,9 @@ ln -sf $HOME/dotfiles/ssh/config $HOME/.ssh/config
 
 #git
 ln -sf $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
+
+#tmux
+ln -sf $HOME/dotfiles/tmux/tmuxconf $HOME/.tmux.conf
+
+#pentadactyl
+ln -sf $HOME/dotfiles/pentadactyl/pentarc $HOME/.pentadactylrc
